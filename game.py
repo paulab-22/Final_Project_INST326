3b5168ee84b1ab8e1019aede9957e3a7ad3044da
Riley <rou1@umd.edu>
2026-04-21 12:45:53 -0400

    Added algorithm skeleton and most of the classes and functions we'll need for the interim deliverable.

---
+++ game.py
@@ -0,0 +1,56 @@
+import random
+
+#Player class
+#also might need a function to check if the player has letters for a word
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.hand = []
+    def add_letter(self, letter):
+        pass
+
+#class for the die
+class Die:
+    def __init__(self, sides):
+        self.sides = sides
+    def roll(self):
+        pass
+    
+class VowelDie(Die):
+    def __init__(self):
+        super().__init__(["B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"])
+        
+
+#Scoring
+class ScoringSystem:
+    def calculate_score(self, words):
+        """calculates score for the turn
+        Algorithm skeleton
+        """
+        total_score = 0
+        
+        #1 loop through word
+        #2 calculate score total based on letter values
+        #3 length multiplier
+        #4 add calculated turn score to toal
+        return total_score
+    
+
+class Game:
+    def __init(self):
+        self.players = []
+        
+    def setup_players(self):
+        pass
+    def play_turn(self):
+        pass
+    def play(self):
+        pass
+    
+def main():
+    game = Game()
+    game.play()
+    
+if __name__ == "__main__":
+    main()
